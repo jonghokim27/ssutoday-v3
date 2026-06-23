8199229ac081b77f0fa1a0351c75d00c5b4f149d
jonghokim27 <joey0307@naver.com>
2026-06-24 00:44:40 +0900

    타임존 설정

--- ssutoday-api/src/main/kotlin/kr/ac/ssu/ssutoday/api/ApiApplication.kt
+++ ssutoday-api/src/main/kotlin/kr/ac/ssu/ssutoday/api/ApiApplication.kt
@@ -1,11 +1,14 @@
 package kr.ac.ssu.ssutoday.api
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.persistence.autoconfigure.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.time.ZoneId
+import java.util.TimeZone
 
 @EnableScheduling
 @EntityScan("kr.ac.ssu.ssutoday.domain")
@@ -16,4 +19,9 @@ class ApiApplication
 
 fun main(args: Array<String>) {
     runApplication<ApiApplication>(*args)
+}
+
+@PostConstruct
+fun setDefaultTimeZone() {
+    TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Seoul")))
 }
