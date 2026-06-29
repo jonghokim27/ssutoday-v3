b036f84c5c7e8f3b84b6b85ac83193957674d291
jonghokim27 <joey0307@naver.com>
2026-06-29 23:13:36 +0900

    모바일 스플래시 아이콘 크기 조정

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -72,7 +72,7 @@ const config = {
       {
         backgroundColor: '#FFFFFF',
         image: './assets/splash-icon.png',
-        imageWidth: 150,
+        imageWidth: 120,
       },
     ],
   ],
